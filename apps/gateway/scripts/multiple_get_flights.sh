109a5d4257313eae805e49900215fdad831ae0fe
Chaimson, Chaya (cb478c) <cb478c@intl.att.com>
2022-10-12 13:19:56 +0300

    add multiple scripts

---
+++ apps/gateway/scripts/multiple_get_flights.sh
@@ -0,0 +1,5 @@
+http get http://localhost:9000/api/flights Cookie:user=chaya --verbose > res1.txt &
+http get http://localhost:9000/api/flights Cookie:user=chaya --verbose > res2.txt &
+http get http://localhost:9000/api/flights Cookie:user=chaya --verbose > res3.txt &
+http get http://localhost:9000/api/flights Cookie:user=chaya --verbose > res4.txt &
+http get http://localhost:9000/api/flights Cookie:user=chaya --verbose > res5.txt
---
+++ apps/gateway/scripts/multiple_get_flights_curl.sh
@@ -0,0 +1,5 @@
+curl -v --cookie "user=chaya" http://localhost:9000/api/flights Cookie:user=chaya;
+curl -v --cookie "user=chaya" http://localhost:9000/api/flights Cookie:user=chaya;
+curl -v --cookie "user=chaya" http://localhost:9000/api/flights Cookie:user=chaya;
+curl -v --cookie "user=chaya" http://localhost:9000/api/flights Cookie:user=chaya;
+curl -v --cookie "user=chaya" http://localhost:9000/api/flights Cookie:user=chaya;
