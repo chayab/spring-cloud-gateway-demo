32094c2b99a2f6a6ee1296a4225aead7ff865c52
Chaimson, Chaya (cb478c) <cb478c@intl.att.com>
2022-10-27 13:26:31 +0300

    improve curl

--- apps/gateway/scripts/multiple_get_flights_curl.sh
+++ apps/gateway/scripts/multiple_get_flights_curl.sh
@@ -1,5 +1,5 @@
-curl -v --cookie "user=chaya" http://localhost:9000/api/flights Cookie:user=chaya;
-curl -v --cookie "user=chaya" http://localhost:9000/api/flights Cookie:user=chaya;
-curl -v --cookie "user=chaya" http://localhost:9000/api/flights Cookie:user=chaya;
-curl -v --cookie "user=chaya" http://localhost:9000/api/flights Cookie:user=chaya;
-curl -v --cookie "user=chaya" http://localhost:9000/api/flights Cookie:user=chaya;
+curl -I --cookie "user=chaya" http://localhost:9000/api/flights Cookie:user=chaya;
+curl -I --cookie "user=chaya" http://localhost:9000/api/flights Cookie:user=chaya;
+curl -I --cookie "user=chaya" http://localhost:9000/api/flights Cookie:user=chaya;
+curl -I --cookie "user=chaya" http://localhost:9000/api/flights Cookie:user=chaya;
+curl -I --cookie "user=chaya" http://localhost:9000/api/flights Cookie:user=chaya;
