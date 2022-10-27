b8227b71042c53eacd5dc217086c9a54acccbac7
Chaimson, Chaya (cb478c) <cb478c@intl.att.com>
2022-10-27 17:59:18 +0300

    add api

--- apps/flights-be/src/main/java/com/chaya/flightsbe/controller/FlightsController.java
+++ apps/flights-be/src/main/java/com/chaya/flightsbe/controller/FlightsController.java
@@ -2,6 +2,7 @@ package com.chaya.flightsbe.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +20,15 @@ public class FlightsController {
 				new Flight("ElAL9897"),
 				new Flight("UAE5463")
 		);
+	}
+
+	@GetMapping("flights/{airport}")
+	public List<Flight> getFlightsByAirports(@PathVariable String airport) {
+		return "TLV".equals(airport) ?
+				List.of(new Flight("WZZ5448"),
+						new Flight("EZ7865")) :
+				List.of(new Flight("ElAL9897"),
+						new Flight("UAE5463"));
+
 	}
 }
